b28bd6b2f89322a4c715fc535c292400d4218b4d
lmn-al <85506134+lmn-al@users.noreply.github.com>
2021-08-07 08:05:34 +0800

    Done Table view display

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
+    
+    var itemArray = ["1", "2", "3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 
 }
 
